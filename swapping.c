5288d3058d4c86b43593793c78ce8b7e06710bec
Jyothipriyanka1220 <69683065+Jyothipriyanka1220@users.noreply.github.com>
2020-08-14 21:53:34 +0530

    Create swapping.c
    swapping of two nums without using temp variables

---
+++ swapping.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a=1;
+int b=2;
+printf("the values before swapping %d,%d:",a,b );
+b=b-a;
+a=a+b;
+printf("a,b values after swaping %d,%d:",a,b);
+}
